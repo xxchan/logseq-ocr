83bb21955fd4629100c40ba83f5c50ad21bd1708
xxchan <xxchan22f@gmail.com>
2023-04-30 19:31:45 +0200

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# logseq-ocr
+
+Read image from clipboard and OCR it!
+
+> **Note**
+>
+> OCR language can be configured in the plugin settings
+
+https://user-images.githubusercontent.com/37948597/235365289-fce2a5a3-a81d-444a-8b8f-91a879c00fde.mov
+
+Credit: inspired by [@laixintao](https://github.com/laixintao) https://www.kawabangga.com/posts/4876
+
+Hint: Other ways to do similar things:
+- From the command line: `pngpaste - | tesseract stdin stdout`
+- [Raycast script command](https://github.com/raycast/script-commands/blob/master/commands/conversions/clipboard-ocr.sh)
+
+This plugin can be slightly more convenient when pasting into Logseq.
---
+++ index.js
@@ -0,0 +1,65 @@
+import { createWorker } from 'tesseract.js';
+import "@logseq/libs"
+
+
+async function pocr(worker) {
+    try {
+        // necessary to have the window focused in order to access the clipboard
+        window.focus();
+
+        const clipboardItem = await navigator.clipboard.read()
+        if (!clipboardItem) {
+            console.error('No clipboard item found')
+            return
+        }
+        console.log('Clipboard item: ', clipboardItem)
+        const data = await clipboardItem[0].getType('image/png').catch(err => {
+            throw new Error('Clipboard item is not an image')
+        })
+        console.log('Clipboard data: ', data)
+
+        const ocrResult = await worker.recognize(
+            data,
+            {},
+            { text: true }
+        )
+        console.log('OCR result: ', ocrResult)
+        return ocrResult.data.text
+    } catch (err) {
+        logseq.UI.showMsg('Failed to read image from clipboard: ' + err, 'error')
+    }
+}
+
+const SETTINGS_SCHEMA = [
+    {
+        key: 'lang',
+        type: 'string',
+        title: 'lang',
+        default: 'eng',
+        description: 'The language for OCR. e.g., `chi_sim+eng` means Simplified Chinese & English. Please check [HERE](https://tesseract-ocr.github.io/tessdoc/Data-Files#data-files-for-version-400-november-29-2016) for supported languages'
+    }
+]
+
+async function main() {
+    logseq.useSettingsSchema(SETTINGS_SCHEMA);
+    const lang = logseq.settings.lang || 'eng'
+
+    const worker = await createWorker();
+    await worker.loadLanguage(lang);
+    await worker.initialize(lang);
+    await worker.setParameters({
+        preserve_interword_spaces: "1",
+    });
+
+    logseq.Editor.registerSlashCommand(
+        'OCR image from clipboard',
+        async () => {
+            const text = await pocr(worker, lang)
+            await logseq.Editor.insertAtEditingCursor(text)
+        },
+    )
+}
+
+
+// bootstrap
+logseq.ready(main).catch(console.error)
